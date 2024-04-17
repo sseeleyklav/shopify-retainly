c4d94a4aa9720a30e548c8874324553e5aa94860
Steve Seeley <steve.seeley@klaviyo.com>
2024-04-17 16:02:39 -0400

    first commit

---
+++ extensions/clp-tooltip/src/BlockExtension.js
@@ -0,0 +1,24 @@
+import {
+  extend,
+  AdminBlock,
+  BlockStack,
+  Text
+} from "@shopify/ui-extensions/admin";
+
+// The target used here must match the target used in the extension's toml file (./shopify.extension.toml)
+const TARGET = 'admin.product-details.block.render';
+
+extend(TARGET, (root, { i18n, data }) => {
+  console.log({data});
+
+  root.append(
+    // The AdminBlock component provides an API for setting the title of the Block extension wrapper.
+    root.createComponent(
+      AdminBlock,
+      { title: "My Block Extension" },
+      root.createComponent(BlockStack, null,
+        root.createComponent(Text, { fontWeight: "bold" }, i18n.translate('welcome', {TARGET}))
+      )
+    )
+  );
+});
